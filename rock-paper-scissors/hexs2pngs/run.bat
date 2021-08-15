8d08c76926ee3608b4d401fe94a1c8506bd4d823
Maarten Pennings <30388931+maarten-pennings@users.noreply.github.com>
2021-08-15 21:10:45 +0200

    4x100 captures with hist eq now as png

--- rock-paper-scissors/hexs2pngs/run.bat
+++ rock-paper-scissors/hexs2pngs/run.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 IF  "(env) "  NEQ  "%PROMPT:~0,6%"  ECHO Please run setup.bat first && EXIT /b
 
-python hexs2pngs.py none.log
-python hexs2pngs.py rock.log
-python hexs2pngs.py paper.log
-python hexs2pngs.py scissors.log
+python hexs2pngs.py ../logs/none.log
+python hexs2pngs.py ../logs/rock.log
+python hexs2pngs.py ../logs/paper.log
+python hexs2pngs.py ../logs/scissors.log
 
